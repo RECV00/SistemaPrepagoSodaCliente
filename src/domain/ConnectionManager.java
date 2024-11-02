e47f5ac8b14d738cca99edf87c6701308b3de756
KeynaCaFu <keynacastrofuentes@gmail.com>
2024-11-02 13:54:05 -0600

    c

--- src/domain/ConnectionManager.java
+++ src/domain/ConnectionManager.java
@@ -9,7 +9,7 @@ import java.net.Socket;
 public class ConnectionManager {
 
 	private String hostServer;
-    private static final int PORT = 12349;
+    private static final int PORT = 12350;
 
     private PrintWriter salida;
     private BufferedReader entrada;
