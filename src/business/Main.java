c53fcc36fc3dd710dd70c2bd6eb7c3d04d6c0327
KeynaCaFu <keynacastrofuentes@gmail.com>
2024-11-02 12:21:39 -0600

    CLIE

--- src/business/Main.java
+++ src/business/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			Parent root = FXMLLoader.load(getClass().getResource("/presentation/UILogin.fxml"));
+			Parent root = FXMLLoader.load(getClass().getResource("/presentation/UILoginClient.fxml"));
 			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
